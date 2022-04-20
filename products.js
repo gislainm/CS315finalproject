1378ce9e1c207d16b1684ddc1bba6bd3502729ee
gislainm <gmuhikira2@gmail.com>
2022-04-20 13:28:18 -0500

    d2commits

---
+++ products.js
@@ -0,0 +1,43 @@
+"use strict";
+/*eslint-disable */
+
+const productsArr = [{
+    name: "Sapiens: A Brief History of Humankind Audible Audiobook – Unabridged Yuval Noah Harari(Author), Derek Perkins(Narrator), HarperAudio(Publisher)",
+    category: "Books",
+    quantity: 3,
+    image: "<img src='./photos/sapiens.jpeg'/>",
+    rating: 5,
+    price: "$18.49"
+},
+{
+    name: "Story Genius: How to Use Brain Science to Go Beyond Outlining and Write a Riveting Novel (Before You Waste Three Years Writing 327 Pages That Go Nowhere)",
+    category: "Books",
+    quantity: 2,
+    image: "<img src='./photos/storyGenius.jpg'/>",
+    rating: 4,
+    price: "$13.79"
+},
+{
+    name: "A Brief History of Time Paperback – Illustrated, ",
+    category: "Books",
+    quantity: 4,
+    image: "<img src='./photos/aBriefHistory.jpg'/>",
+    rating: 5,
+    price: "$15.44"
+},
+{
+    name: "The Shining Mass Market Paperback – June 26, 2012",
+    category: "Books",
+    quantity: 5,
+    image: "<img src='./photos/theShining.jpg'/>",
+    rating: 5,
+    price: "$11.49"
+},
+{
+    name: "Dune (Dune Chronicles, Book 1) Paperback – August 2",
+    category: "Books",
+    quantity: 10,
+    image: "<img src='./photos/dube.jpg'/>",
+    rating: 5,
+    price: "$20.79"
+}]
